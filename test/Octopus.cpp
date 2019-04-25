3b00681c12b24436173798c77abcda3a87f95ab5
Nosuriba <tekito>
2019-04-25 13:03:17 +0900

    複数の線(関節の挙動?)の計算と移動を仮設定した

--- test/Octopus.cpp
+++ test/Octopus.cpp
@@ -51,9 +51,11 @@ void Octopus::Update( const Vector2f& plPos)
 
 void Octopus::CalTrigonometric(const Vector2f & pos)
 {
-	/// 単体の計算
-	auto A =  pos - _sPoint;
-	auto B = _ePoint - _sPoint;
+	auto A = Vector2f();
+	auto B = Vector2f();
+	/*/// 単体の計算	
+	A =  pos - _sPoint;
+	B = _ePoint - _sPoint;
 	A.Normalize();
 	B.Normalize();
 
@@ -61,15 +63,44 @@ void Octopus::CalTrigonometric(const Vector2f & pos)
 	sin = 1.0 - abs(cos);
 	
 	sin = (_sPoint.y - pos.y > 0.f ? sin : -sin);
+*/
+	/// 複数の計算
+	auto node = nodes.end() - 1;
+	for (; node != nodes.begin(); --node)
+	{
+		A = pos - (*node)._sPoint;
+		B = (*node)._ePoint - (*node)._sPoint;
+		A.Normalize();
+		B.Normalize();
+
+		(*node).cos = Dot(A, B);
+		(*node).sin = 1.0 - abs((*node).cos);
+		
+		(*node).sin = ((*node)._sPoint.y - pos.y > 0.f ? (*node).sin : -(*node).sin);
+	}
+
 
 }	
 
 void Octopus::Move()
 {
-	auto d = _ePoint - _sPoint;				/// 終点から始点のまでの距離
+	auto d = Vector2f();
+
+	/// 単体の移動
+	/*d = _ePoint - _sPoint;				/// 終点から始点のまでの距離
 	
 	debugPoint.x = _sPoint.x + ((d.x * cos) - (d.y * sin));
 	debugPoint.y = _sPoint.y - ((d.x * sin) + (d.y * cos));
+*/
+	/// 複数の移動
+	auto node = nodes.end() - 1;
+	for (; node != nodes.begin(); --node)
+	{
+		d = (*node)._ePoint - (*node)._sPoint;
+		(*node)._debugPoint.x = (*node)._sPoint.x + ((d.x * (*node).cos) - (d.y * (*node).sin));
+		(*node)._debugPoint.y = (*node)._sPoint.y - ((d.x * (*node).sin) + (d.y * (*node).cos));
+	}
+
 }
 
 void Octopus::DebugDraw()
@@ -95,7 +126,7 @@ void Octopus::DebugDraw()
 		//DrawLine(node._sPoint.x, node._sPoint.y, node._debugPoint.x, node._debugPoint.y, 0x00ff00);
 
 		DrawCircle(node._ePoint.x, node._ePoint.y, 5, 0xff8800, true);
-		DrawCircle(node._debugPoint.x, node._debugPoint.y, 10, 0xffff00, true);
+		DrawCircle(node._debugPoint.x, node._debugPoint.y, 5, 0xffff00, true);
 
 	}
 	//DrawLine(_sPoint.x, _sPoint.y, _pos.x, _pos.y, 0xff0000);
@@ -105,7 +136,7 @@ void Octopus::DebugDraw()
 
 	DrawFormatString(0, 0, 0xffffff, "cos : %f", cos);
 	DrawFormatString(0, 30, 0xffffff, "sin : %f", sin);
-	/*DrawFormatString(200, 0, 0xffffff, "始点      : X座標 %f, Y座標 %f", _sPoint.x, _sPoint.y);
+	/*DrawFormatString(200, 0, 0xffffff, "始点    : X座標 %f, Y座標 %f", _sPoint.x, _sPoint.y);
 	DrawFormatString(200, 30, 0xffffff, "終点     : X座標 %f, Y座標 %f", _ePoint.x, _ePoint.y);
 	DrawFormatString(200, 60, 0xffffff, "CalPoint : X座標 %f, Y座標 %f", debugPoint.x, debugPoint.y);*/
 	DrawFormatString(200, 90, 0xffffff, "ﾌﾟﾚｲﾔｰ   : X座標 %f, Y座標 %f", _pos.x, _pos.y);
